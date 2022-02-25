36e5a6d7528a1961d37101e83ec331d763bd9014
robmikh <rob.mikh@outlook.com>
2022-02-25 14:35:57 -0800

    Add project files.

---
+++ PackagingRepro/PackagingRepro/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinRTInteropTools;
+
+namespace PackagingRepro
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Trying...";
+            try
+            {
+                var device = new Direct3D11Device();
+            }
+            catch (Exception ex)
+            {
+                myButton.Content = "Failure!";
+                return;
+            }
+            myButton.Content = "It worked!";
+        }
+    }
+}
